2f9936043f722af0e5c3e6d29df44e1e14ffa911
Darlo Miguel Ilagan <d.ilagan@sparkles.com.ph>
2023-04-28 03:51:53 +0800

    updated history

--- src/SalesHistory.js
+++ src/SalesHistory.js
@@ -5,16 +5,64 @@ import { getSalesHistory } from "./localStorageHelper";
 const SalesHistory = () => {
   const salesHistory = getSalesHistory();
 
+  const formatDate = (date) => {
+    return new Date(date).toLocaleDateString();
+  };
+
+  const formatTime = (date) => {
+    return new Date(date).toLocaleTimeString();
+  };
+
+  const dailyIncome = salesHistory.reduce((income, sale) => {
+    const date = formatDate(sale.date);
+    income[date] = (income[date] || 0) + sale.total;
+    return income;
+  }, {});
+
   return (
     <div className="sales-history">
       <h3>Sales History</h3>
-      <ul>
-        {salesHistory.map((sale, index) => (
-          <li key={index}>
-            {sale.date} - {sale.items.map((item) => item.name).join(", ")}
-          </li>
-        ))}
-      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Items</th>
+            <th>Total</th>
+            <th>Paid</th>
+            <th>Change</th>
+          </tr>
+        </thead>
+        <tbody>
+          {salesHistory.map((sale, index) => (
+            <tr key={index}>
+              <td>{formatDate(sale.date)}</td>
+              <td>{formatTime(sale.date)}</td>
+              <td>{sale.items.map((item) => item.name).join(", ")}</td>
+              <td>₱{sale.total.toLocaleString("en-PH", { minimumFractionDigits: 2 })}</td>
+              <td>₱{sale.paid.toLocaleString("en-PH", { minimumFractionDigits: 2 })}</td>
+              <td>₱{sale.change.toLocaleString("en-PH", { minimumFractionDigits: 2 })}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <h3>Daily Income</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Income</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(dailyIncome).map(([date, income]) => (
+            <tr key={date}>
+              <td>{date}</td>
+              <td>₱{income.toLocaleString("en-PH", { minimumFractionDigits: 2 })}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
